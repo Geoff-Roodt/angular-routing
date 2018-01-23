d6f507ae4868f399f3d568e28d19e73bcab2506b
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-01-24 09:40:35 +1300

    Added basic routing app and set it up for routing

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: 'HomeComponent'},
+  {path: 'about', component: 'AboutComponent'},
+  {path: 'contact', component: 'ContactComponent'},
+  {path: 'contactus', redirectTo: 'contact'},
+
+  // required for the authentication demo
+  {path: 'login', component: LoginComponent},
+  {path:'protected', component: ProtectedComponent, canActivate: [LoggedInGuard]},
+  {path:'products', component: ProductsComponent, children: childRoutes}
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    {provide: LocationStrategy, useClass: HashLocationStrategy},
+    {provide: APP_BASE_HREF, useValue: '/'}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+
+export class AppModule {
+
+
+}
