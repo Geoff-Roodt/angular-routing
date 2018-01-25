a981956f478717cb9c6ed809e742ea93b5ffa802
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-01-26 09:30:23 +1300

    Added child components, code and html to follow

--- routing/src/app/products/products.module.ts
+++ routing/src/app/products/products.module.ts
@@ -1,5 +1,11 @@
-import {Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes, ActivatedRoute, Router} from '@angular/router';
+
 import {ProductsComponent} from './products.component';
+import {MainComponent} from './main/main.component';
+import {MoreInfoComponent} from './more-info/more-info.component';
+import {ProductComponent} from './product/product.component';
 
 
 export const routes: Routes = [
@@ -7,5 +13,25 @@ export const routes: Routes = [
   {path: 'main', component: MainComponent},
   {path: 'more-info', component: MoreInfoComponent},
   {path: ':id', component: ProductsComponent},
-
 ];
+
+@ngModule({
+  declarations:[
+    ProductsComponent,
+    ProductComponent,
+    MainComponent,
+    MoreInfoComponent
+  ],
+  exports: [
+    ProductsComponent,
+    ProductComponent,
+    MainComponent,
+    MoreInfoComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+
+export class ProductsModule { }
